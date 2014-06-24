c9f97c9c9e5f404951eec4f7119c21f3bbb65a4d
pahimar <pahimar@gmail.com>
2014-06-24 18:40:17 -0400

    Committing changes from episode 3 and 4

---
+++ src/main/java/com/pahimar/letsmodreboot/LetsModReboot.java
@@ -0,0 +1,31 @@
+package com.pahimar.letsmodreboot;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "LetsModReboot", name = "Lets Mod Reboot", version = "1.7.2-1.0")
+public class LetsModReboot
+{
+    @Mod.Instance("LetsModReboot")
+    public static LetsModReboot instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
