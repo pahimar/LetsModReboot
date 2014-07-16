17337e9455505f959f86f4f457f0823cd82f4d5a
pahimar <pahimar@gmail.com>
2014-07-15 20:31:18 -0400

    Changes for episode 11

--- src/main/java/com/pahimar/letsmodreboot/creativetab/CreativeTabLMRB.java
+++ src/main/java/com/pahimar/letsmodreboot/creativetab/CreativeTabLMRB.java
@@ -7,18 +7,12 @@ import net.minecraft.item.Item;
 
 public class CreativeTabLMRB
 {
-    public static final CreativeTabs LMRB_TAB = new CreativeTabs(Reference.MOD_ID)
+    public static final CreativeTabs LMRB_TAB = new CreativeTabs(Reference.MOD_ID.toLowerCase())
     {
         @Override
         public Item getTabIconItem()
         {
             return ModItems.mapleLeaf;
-        }
-
-        @Override
-        public String getTranslatedTabLabel()
-        {
-            return "Let's Mod Reboot";
         }
     };
 }
